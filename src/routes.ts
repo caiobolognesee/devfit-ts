f18c0995c44bdcb7be05b6ff4ac7024096a98db0
Caio <caio.bolognese@greenn.com.br>
2026-02-15 22:34:12 -0300

    token validation middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,19 @@
 import { FastifyInstance } from "fastify";
 import { container } from "./container";
+import { authMiddleware } from "./middlewares/auth.middleware";
 
 export async function registerRoutes(app: FastifyInstance) {
   app.register(async (userApp) => {
     userApp.post("/", container.userController.create.bind(container.userController));
+
+    userApp.get(
+      "/me",
+      { preHandler: authMiddleware },
+      async (request) => {
+        return (request as any).user;
+      }
+    );
+
   }, { prefix: "/users" });
 
   app.register(async (authApp) => {
